e83e583d5324aa62da2edc33da2f3fbd021c5c78
098jayasurya <jayanachaka@gmail.com>
2026-05-18 12:02:22 +0530

    2nd

---
+++ college.java
@@ -0,0 +1,36 @@
+class employee{
+    int empId;
+    string emp_name;
+    long emp_num;
+    double emp_sal;
+}
+
+class company{
+    public static void main(String[] args){
+        employee e1 = new employee():
+        employee e2 = new employee();
+
+        e1.empId = 1;
+        e1.emp_name ="jay";
+        e1.emp_num ="934223422"
+        e1.empsal =5000.33;
+
+        e2.empId = 2;
+        e2.emp_name ="surya";
+        e2.emp_num ="733722131"
+        e2.empsal =649004;
+
+        System.out.println(e1.empId);
+        System.out.println(e1.emp_name);
+        System.out.println(e1.emp_num);
+        System.out.println(e1.emp_sal);
+
+        System.out.println();
+
+        System.out.println(e2.empId);
+        System.out.println(e2.emp_name);
+        System.out.println(e2.emp_num);
+        System.out.println(e2.emp_sal);
+
+    }
+}
