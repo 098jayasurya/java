6695ad9b4385465382e49b898ac41aa0c5ea58a2
098jayasurya <jayanachaka@gmail.com>
2026-05-18 11:52:28 +0530

    1st

--- project1.java
+++ project1.java
@@ -1,3 +1,25 @@
-class Studen{
-    
+class Student01{
+    String name ="jaya";
+    int rollNumber=90;
+    long[] phoneNum={9381144674,555435345};
+    String sclass="1OTH ";
+
+}
+class Student02{
+    String name ="jaya";
+    int rollNumber=90;
+    long[] phoneNum={9381144674,555435345};
+    String sclass="1OTH ";
+
+}
+class Test01_student{
+    public static void main(String[] args){
+    Student01 s1 = new Student01();
+    Student02 s2 = new Student02();
+
+    System.out.println("s1.name"+s1.name);
+    System.out.println(s1.rollNumber);
+    System.out.println(s1.phoneNum[0]);
+    System.out.println(s1.sclass);
+}
 }
