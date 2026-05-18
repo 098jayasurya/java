b4d0b6afec86a3af23e5d79a853f8f3b2feb51fd
098jayasurya <jayanachaka@gmail.com>
2026-05-18 16:34:08 +0530

    scanner

---
+++ addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class addition {
+    public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    System.out.println("Enter your f no :");
+    int i = scn.nextInt();
+    System.out.println("Enter your secind num :");
+    int j = scn.nextInt();
+
+    System.out.println(i + j);
+
+    }
+}
--- company.java
+++ company.java
@@ -1,24 +1,24 @@
 class employee{
     int empId;
-    string emp_name;
+    String emp_name;
     long emp_num;
     double emp_sal;
 }
 
 class company{
     public static void main(String[] args){
-        employee e1 = new employee():
+        employee e1 = new employee();
         employee e2 = new employee();
 
         e1.empId = 1;
         e1.emp_name ="jay";
-        e1.emp_num ="934223422"
-        e1.empsal =5000.33;
+        e1.emp_num =934223422;
+        e1.emp_sal =5000.33;
 
         e2.empId = 2;
         e2.emp_name ="surya";
-        e2.emp_num ="733722131"
-        e2.empsal =649004;
+        e2.emp_num =733722131;
+        e2.emp_sal =649004;
 
         System.out.println(e1.empId);
         System.out.println(e1.emp_name);
