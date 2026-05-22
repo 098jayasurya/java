c4c6353ee8b101cfbd2e4a6a3bc7eab0fde08421
098jayasurya <jayanachaka@gmail.com>
2026-05-22 11:40:36 +0530

    iff

--- conditinals/iff.java
+++ conditinals/iff.java
@@ -1,16 +1,31 @@
 package conditinals;
 
 public class iff {
+    // static void m1(int a){
+    //     System.out.println("HI");
+
+    //     if(a==10){
+    //         System.out.println("Hello");
+    //     }
+    //     System.out.println("HRU\n");
+    // }
+
     static void m1(int a){
-        System.out.println("HI");
+        if(a == 5){
+            System.out.println("hi1");
+
+        }
+        System.out.println("hello1");
 
         if(a==10){
-            System.out.println("Hello");
+            System.out.println("hi2");
         }
-        System.out.println("HRU\n");
+        System.out.println("Hello2\n");
     }
     public static void main(String[] args) {
+        m1(5);
         m1(10);
-        m1(15);
+        m1(20);
+
     }
 }
