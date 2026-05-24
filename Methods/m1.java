207ec31288a1137aa41b2f5e5c034bb98c4d056e
098jayasurya <jayanachaka@gmail.com>
2026-05-24 14:36:27 +0530

    m1

---
+++ Methods/m1.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class m1 {
+    static int  m2(int a,int b){
+    return a + b;
+    }
+    public static void main(String[] args) {
+        
+        var res = m2(8,3);
+        System.out.println(res);
+    }
+}
