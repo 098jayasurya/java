994afb2f86a01578e8a78798dc09f758a1b44669
098jayasurya <jayanachaka@gmail.com>
2026-05-25 19:34:32 +0530

    this

---
+++ thh/th1.java
@@ -0,0 +1,14 @@
+package thh;
+
+public class th1{
+    int x = 10;
+
+    void m1(){
+        System.out.println(x);
+    }
+public static void main(String[] args) {
+        th1 t1 = new th1();
+        t1.m1();
+
+    }
+}
