ae42f4aac2160b2a03af0bd0a131de1ba533880b
098jayasurya <jayanachaka@gmail.com>
2026-07-20 14:32:29 +0530

    innerclasses

--- innerclasses/Sample.java
+++ innerclasses/Sample.java
@@ -1,5 +1,19 @@
 package innerclasses;
 
 public class Sample {
-    
+    class B{
+        void m1(){
+            System.out.println("B m1");
+        }
+    }
+    void m2(){
+        B b = new B();
+        b.m1();
+        }
+
+    public static void main(String[] args) {
+        //B b = new B();
+        B b = new Sample().new B();
+        b.m1();
+    }    
 }
